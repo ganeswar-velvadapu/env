f391f8bfc161fa540003f663023148e04ebeb6e8
ganeswar-velvadapu <ganeshvelvadapu@gmail.com>
2025-04-21 11:27:56 +0530

    reports model

--- frontend/lib/models/report.dart
+++ frontend/lib/models/report.dart
@@ -1,37 +1,29 @@
 class Report {
   final String title;
   final String description;
-  final String status;
   final String location;
-  final String userId;
+  final String status;
+  final int userId;
+  final String email;
 
   Report({
     required this.title,
     required this.description,
-    required this.status,
     required this.location,
+    required this.status,
     required this.userId,
+    required this.email,
   });
 
-  // Convert from JSON (from API to Dart object)
-  factory Report.fromJson(Map<String, dynamic> json) {
+  
+  factory Report.fromMap(Map<String, dynamic> map) {
     return Report(
-      title: json['title'],
-      description: json['description'],
-      status: json['status'],
-      location: json['location'],
-      userId: json['user_id'].toString(), 
+      title: map['title'] ?? '',
+      description: map['description'] ?? '',
+      location: map['location'] ?? '',
+      status: map['status'] ?? '',
+      userId: map['user_id'],
+      email: map['email'] ?? '',
     );
-  }
-
-  // Convert to JSON (from Dart object to send to API)
-  Map<String, dynamic> toJson() {
-    return {
-      "title": title,
-      "description": description,
-      "status": status,
-      "location": location,
-      "user_id": userId,
-    };
   }
 }
