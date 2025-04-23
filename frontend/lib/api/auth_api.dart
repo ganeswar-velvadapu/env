853c1bfe32ea8eb0392099406434b1a2e2a7f472
ganeswar-velvadapu <ganeshvelvadapu@gmail.com>
2025-04-23 11:10:40 +0530

    ip address change

--- frontend/lib/api/auth_api.dart
+++ frontend/lib/api/auth_api.dart
@@ -1,7 +1,7 @@
 import 'package:dio/dio.dart';
 
 class AuthApi {
-  static const String baseUrl = 'http://192.168.23.186:8000/api/auth';
+  static const String baseUrl = 'http://192.168.0.102:8000/api/auth';
   static final Dio dio = Dio(BaseOptions(baseUrl: baseUrl));
 
   static Future<Response> login(String email, String password) async {
--- frontend/lib/api/report_api.dart
+++ frontend/lib/api/report_api.dart
@@ -1,29 +1,53 @@
 import 'package:dio/dio.dart';
 
 class ReportApi {
-  static const String BaseUrl = "http://192.168.23.186:8000/api";
+  static const String BaseUrl = "http://192.168.0.102:8000/api";
   static final Dio dio = Dio(BaseOptions(baseUrl: BaseUrl));
 
   static Future<Response> getAllReports() async {
     return await dio.get("/report");
   }
 
- static Future<Response> addReport(Map<String, dynamic> reportData, String token) async {
-  try {
-    final response = await dio.post(
-      '/report',
-      data: reportData,
-      options: Options(
-        headers: {
-          'Authorization': 'Bearer $token',
-        },
-      ),
-    );
-    return response;
-  } catch (e) {
-    print('Error adding report: $e');
-    rethrow;
+  static Future<Response> addReport(
+    Map<String, dynamic> reportData,
+    String token,
+  ) async {
+    try {
+      final response = await dio.post(
+        '/report',
+        data: reportData,
+        options: Options(headers: {'Authorization': 'Bearer $token'}),
+      );
+      return response;
+    } catch (e) {
+      print('Error adding report: $e');
+      rethrow;
+    }
   }
+
+  static Future<Response> getUserReports(String token) async {
+    return await dio.get(
+      '/user/report',
+      options: Options(headers: {'Authorization': 'Bearer $token'}),
+    );
+  }
+
+  static Future<void> deleteReport(int reportId, String token) async {
+    await dio.delete(
+      '/report/$reportId',
+      options: Options(headers: {'Authorization': 'Bearer $token'}),
+    );
+  }
+
+  static Future<Response> updateReport(int id, Map<String, dynamic> updatedData, String token) async {
+  return await dio.put(
+    '/report/$id',
+    data: updatedData,
+    options: Options(headers: {
+      'Authorization': 'Bearer $token',
+    }),
+  );
 }
+
 
 }
