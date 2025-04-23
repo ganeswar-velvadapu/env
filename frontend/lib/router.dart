96ea88a608e62b3b0d8ba99f340010ffed2f0986
ganeswar-velvadapu <ganeshvelvadapu@gmail.com>
2025-04-23 11:10:17 +0530

    added user reports route

--- frontend/lib/router.dart
+++ frontend/lib/router.dart
@@ -2,6 +2,8 @@ import 'package:frontend/layout.dart';
 import 'package:frontend/screens/add_a_report.dart';
 import 'package:frontend/screens/home_screen.dart';
 import 'package:frontend/screens/login_screen.dart';
+import 'package:frontend/screens/map_ngos.dart';
+import 'package:frontend/screens/my_reports.dart';
 import 'package:frontend/screens/profile_screen.dart';
 import 'package:frontend/screens/signup_screen.dart';
 import 'package:go_router/go_router.dart';
@@ -10,12 +12,44 @@ GoRouter createRouter(String initialLocation) {
   return GoRouter(
     initialLocation: initialLocation,
     routes: [
-      GoRoute(path: '/', name: "home", builder: (context, state) => const Layout()),
-      GoRoute(path: '/reports', name: "homescreen", builder: (context, state) => const HomeScreen()),
-      GoRoute(path: '/login', name: "login", builder: (context, state) => const LoginScreen()),
-      GoRoute(path: '/signup', name: "signup", builder: (context, state) => const SignupScreen()),
-      GoRoute(path: '/profile', name: "profile", builder: (context, state) => ProfileScreen()),
-      GoRoute(path: '/report/add',name: "add report",builder: (context,state)=> AddReportScreen())
+      GoRoute(
+        path: '/',
+        name: "home",
+        builder: (context, state) => const Layout(),
+      ),
+      GoRoute(
+        path: '/reports',
+        name: "homescreen",
+        builder: (context, state) => const HomeScreen(),
+      ),
+      GoRoute(
+        path: '/login',
+        name: "login",
+        builder: (context, state) => const LoginScreen(),
+      ),
+      GoRoute(
+        path: '/signup',
+        name: "signup",
+        builder: (context, state) => const SignupScreen(),
+      ),
+      GoRoute(
+        path: '/profile',
+        name: "profile",
+        builder: (context, state) => ProfileScreen(),
+      ),
+      GoRoute(
+        path: '/report/add',
+        name: "add report",
+        builder: (context, state) => AddReportScreen(),
+      ),
+      GoRoute(
+        path: '/my-reports',
+        builder: (context, state) => const MyReportsScreen(),
+      )
+      ,GoRoute(
+        path: '/mapngo',
+        builder: (context, state) => const MapNGOs(),
+      ),
     ],
   );
 }
