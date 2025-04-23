51edd4109449072bf5dad29b5daf7e82d833d799
ganeswar-velvadapu <ganeshvelvadapu@gmail.com>
2025-04-23 11:12:39 +0530

    added map ngos page

---
+++ frontend/lib/screens/map_ngos.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MapNGOs extends StatelessWidget {
+  const MapNGOs({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Coming Soom"),
+      ),
+    );
+  }
+}
