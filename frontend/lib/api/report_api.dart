313ac4c8fa95334094841a762808016ed08c065c
ganeswar-velvadapu <ganeshvelvadapu@gmail.com>
2025-04-23 08:39:44 +0530

    added addReport function

--- frontend/lib/api/report_api.dart
+++ frontend/lib/api/report_api.dart
@@ -1,17 +1,29 @@
 import 'package:dio/dio.dart';
 
 class ReportApi {
-
   static const String BaseUrl = "http://192.168.23.186:8000/api";
   static final Dio dio = Dio(BaseOptions(baseUrl: BaseUrl));
 
-
-  static Future<Response> getAllReports()  async{
+  static Future<Response> getAllReports() async {
     return await dio.get("/report");
   }
 
-    static Future<Response> postReport(Map<String, dynamic> reportData) async {
-    return await dio.post("/report", data: reportData);
+ static Future<Response> addReport(Map<String, dynamic> reportData, String token) async {
+  try {
+    final response = await dio.post(
+      '/report',
+      data: reportData,
+      options: Options(
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+      ),
+    );
+    return response;
+  } catch (e) {
+    print('Error adding report: $e');
+    rethrow;
   }
+}
 
 }
