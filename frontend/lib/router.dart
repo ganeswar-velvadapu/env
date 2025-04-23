e004f6df79e2dd2013f284faa50c3b93ff7246ef
ganeswar-velvadapu <ganeshvelvadapu@gmail.com>
2025-04-23 08:39:25 +0530

    changed name

--- frontend/lib/router.dart
+++ frontend/lib/router.dart
@@ -1,3 +1,5 @@
+import 'package:frontend/layout.dart';
+import 'package:frontend/screens/add_a_report.dart';
 import 'package:frontend/screens/home_screen.dart';
 import 'package:frontend/screens/login_screen.dart';
 import 'package:frontend/screens/profile_screen.dart';
@@ -8,10 +10,12 @@ GoRouter createRouter(String initialLocation) {
   return GoRouter(
     initialLocation: initialLocation,
     routes: [
-      GoRoute(path: '/', name: "home", builder: (context, state) => const HomeScreen()),
+      GoRoute(path: '/', name: "home", builder: (context, state) => const Layout()),
+      GoRoute(path: '/reports', name: "homescreen", builder: (context, state) => const HomeScreen()),
       GoRoute(path: '/login', name: "login", builder: (context, state) => const LoginScreen()),
       GoRoute(path: '/signup', name: "signup", builder: (context, state) => const SignupScreen()),
       GoRoute(path: '/profile', name: "profile", builder: (context, state) => ProfileScreen()),
+      GoRoute(path: '/report/add',name: "add report",builder: (context,state)=> AddReportScreen())
     ],
   );
 }
