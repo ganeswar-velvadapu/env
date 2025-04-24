0cec58f129e991722a9173d6601bbedfb72514b8
ganeswar-velvadapu <ganeshvelvadapu@gmail.com>
2025-04-25 01:29:40 +0530

    added events route

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 from routes.auth import auth_router
 from routes.reports import reports_router
-
+from routes.events import events_router
 
 app = FastAPI()
 
 
 app.include_router(auth_router)
 app.include_router(reports_router)
+app.include_router(events_router)
 
 @app.get("/")
 def home():
