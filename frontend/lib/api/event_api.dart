f5353d6b5e55538a14db6dd4f472457633db089d
ganeswar-velvadapu <ganeshvelvadapu@gmail.com>
2025-04-28 22:25:36 +0530

    added events api

---
+++ frontend/lib/api/event_api.dart
@@ -0,0 +1,61 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class EventApi {
+  static String baseUrl = '${dotenv.env["BACKEND_BASE_URL"]}/api';
+  static final Dio dio = Dio(BaseOptions(baseUrl: baseUrl));
+
+  // Fetch all events
+  static Future<Response> getAllEvents() async {
+    return await dio.get('/events');
+  }
+
+  // Add a new event
+  static Future<Response> addEvent(
+    Map<String, dynamic> eventData,
+    String token,
+  ) async {
+    try {
+      final response = await dio.post(
+        '/events',
+        data: eventData,
+        options: Options(headers: {'Authorization': 'Bearer $token'}),
+      );
+      return response;
+    } catch (e) {
+      print('Error adding event: $e'); 
+      rethrow;
+    }
+  }
+
+  static Future<Response> editEvent(
+    Map<String, dynamic> eventData,
+    String token,
+    int id,
+  ) async {
+    try {
+      final response = await dio.put(
+        '/events/$id',
+        data: eventData,
+        options: Options(headers: {'Authorization': 'Bearer $token'}),
+      );
+      return response;
+    } catch (e) {
+      print('Error editing event: $e');
+      rethrow;
+    }
+  }
+
+  static Future<Response> deleteEvent(String token, int id) async {
+    try {
+      final response = await dio.delete(
+        '/events/$id',
+        options: Options(headers: {'Authorization': 'Bearer $token'}),
+      );
+      return response;
+    } catch (e) {
+      print('Error deleting event: $e');
+      rethrow;
+    }
+  }
+}
