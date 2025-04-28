5692e73b9078ed08f36012dbaeaed1c26f9e966e
ganeswar-velvadapu <ganeshvelvadapu@gmail.com>
2025-04-28 21:47:29 +0530

    added enum user type and parsed it to enum based on given string input

--- frontend/lib/models/user.dart
+++ frontend/lib/models/user.dart
@@ -2,14 +2,33 @@ class User {
   final String id;
   final String email;
   final String token;
+  final UserType userType;
 
-  User({required this.id, required this.email, required this.token});
+  User({required this.id, required this.email, required this.token, required this.userType});
 
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
       id: json['id'].toString(),
       email: json['email'],
       token: json['token'],
+      userType: _parseUserType(json['user_type']),
     );
   }
+
+  static UserType _parseUserType(String userTypeString) {
+    switch (userTypeString.toLowerCase()) {
+      case 'ngo':
+        return UserType.ngo;
+      case 'normal':
+        return UserType.normal;
+      default:
+        throw Exception('Unknown user type: $userTypeString');
+    }
+  }
+
+}
+
+enum UserType {
+  normal,
+  ngo,
 }
